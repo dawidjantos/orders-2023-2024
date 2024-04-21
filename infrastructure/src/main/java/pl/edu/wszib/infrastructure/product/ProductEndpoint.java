878dc54c6752d38c0d504d4263ec9c9e14724feb
ppustelnik <pawel.pustelnik@exorigo-upos.pl>
2024-04-21 16:07:03 +0200

    impl tasks

--- infrastructure/src/main/java/pl/edu/wszib/infrastructure/product/ProductEndpoint.java
+++ infrastructure/src/main/java/pl/edu/wszib/infrastructure/product/ProductEndpoint.java
@@ -1,9 +1,10 @@
 package pl.edu.wszib.infrastructure.product;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import pl.edu.wszib.api.product.ProductApi;
+import pl.edu.wszib.api.product.ProductResult;
 import pl.edu.wszib.application.product.ProductFacade;
 
 import java.util.Collection;
@@ -11,9 +12,6 @@ import java.util.Collection;
 @RestController
 @RequestMapping("/products")
 public class ProductEndpoint {
-    // TODO:
-    //  - implementacja pobierania produktu na podstawie id GET /products/{productId}
-    //  - tworzenie produktu POST /products
     private final ProductFacade productFacade;
 
     public ProductEndpoint(final ProductFacade productFacade) {
@@ -23,6 +21,34 @@ public class ProductEndpoint {
     @GetMapping
     public Collection<ProductApi> getAll() {
         return productFacade.getAll();
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<?> getById(@PathVariable("productId") String productId) {
+        final ProductResult result = productFacade.getById(productId);
+        if (result.isSuccess()) {
+            return ResponseEntity.ok(result.success());
+        } else {
+            if (result.code() == ProductResult.Code.NOT_FUND) {
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.internalServerError().body(result);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody final ProductApi product) {
+        final ProductResult result = productFacade.create(product);
+        if (result.isSuccess()) {
+            return ResponseEntity.ok(result.success());
+        } else {
+            if (result.code() == ProductResult.Code.ALREADY_EXISTS) {
+                return ResponseEntity
+                        .status(HttpStatusCode.valueOf(409))
+                        .body(result);
+            }
+            return ResponseEntity.internalServerError().body(result);
+        }
     }
 
 }
